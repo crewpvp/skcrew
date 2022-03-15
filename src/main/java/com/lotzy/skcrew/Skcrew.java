6aa151f07afe3af08a9ec25bd918121510d7574c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 04:06:39 +0300

    add checks for server platform and needed plugins

--- src/main/java/com/lotzy/skcrew/Skcrew.java
+++ src/main/java/com/lotzy/skcrew/Skcrew.java
@@ -29,12 +29,23 @@ public final class Skcrew extends JavaPlugin {
         config = YamlConfiguration.loadConfiguration(configFile);
         try {
             if  (config.getBoolean("files.enabled")) addonInstance.loadClasses("com.lotzy.skcrew.file"); 
-            if  (config.getBoolean("paper.enabled")) addonInstance.loadClasses("com.lotzy.skcrew.paper"); 
+            if  (config.getBoolean("paper.enabled")) {
+                if (Skript.classExists("com.destroystokyo.paper.event.player.PlayerStartSpectatingEntityEvent")
+                        && Skript.classExists("com.destroystokyo.paper.event.player.PlayerStopSpectatingEntityEvent")) {
+                    addonInstance.loadClasses("com.lotzy.skcrew.paper");
+                } else {
+                    Bukkit.getLogger().info("[Skcrew] Server platform doesn't support paper events, disabling 'paper' functional");
+                }
+            } 
             if  (config.getBoolean("other.enabled")) addonInstance.loadClasses("com.lotzy.skcrew.other"); 
             if  (config.getBoolean("map.enabled"))   addonInstance.loadClasses("com.lotzy.skcrew.map"); 
             if  (config.getBoolean("floodgate.enabled")) {
-                addonInstance.loadClasses("com.lotzy.skcrew.floodgate");
-                Bukkit.getServer().getPluginManager().registerEvents(new FormEvents(), this);
+                if (Bukkit.getPluginManager().getPlugin("Floodgate").isEnabled()) {
+                    addonInstance.loadClasses("com.lotzy.skcrew.floodgate");
+                    Bukkit.getServer().getPluginManager().registerEvents(new FormEvents(), this);
+                } else {
+                    Bukkit.getLogger().info("[Skcrew] Floodgate is not installed, disabling 'floodgate' support");
+                }
             }
             if  (config.getBoolean("interpretate.enabled")) addonInstance.loadClasses("com.lotzy.skcrew.interpretate"); 
             if  (config.getBoolean("runtime.enabled"))      addonInstance.loadClasses("com.lotzy.skcrew.runtime"); 
