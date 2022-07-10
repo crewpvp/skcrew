a96dd67742fa43fe67de76354a433fc24e25144a
Lotzyprod <trek.ru@yandex.ru>
2022-07-10 07:58:55 +0300

    Fix version literal :)

--- src/main/java/com/lotzy/skcrew/via/ExprPlayerVersion.java
+++ src/main/java/com/lotzy/skcrew/via/ExprPlayerVersion.java
@@ -56,7 +56,7 @@ public class ExprPlayerVersion extends SimpleExpression<String> {
         } else if (ver < 757) {
             return new String[] {"1.17.1"};
         } else if (ver < 758) {
-            return new String[] {"1.18.2+"};
+            return new String[] {"1.18.2"};
         } else {
             return new String[] {"1.19+"};
         }
