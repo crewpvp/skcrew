2cc1d1cb9f507766bf34aa1037afc0521cbc98ab
Lotzyprod <trek.ru@yandex.ru>
2023-10-07 20:55:08 +0300

    new socket client implementation

---
+++ src/main/java/com/lotzy/skcrew/spigot/sockets/SocketClient.java
@@ -0,0 +1,149 @@
+package com.lotzy.skcrew.spigot.sockets;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class SocketClient implements Runnable {
+    public interface ClientListener {
+        default public void onConnection(InetSocketAddress address) {};
+        default public void onDisconnection() {};
+        default public void onReceivePacket(Object object) {};
+        default public void onSendPacket(Object object, boolean sended) {};
+        default public void onInvalidPacket() {};
+        default public void onReconnection() {};
+        default public void onClose() {};
+    }
+    
+    String host;
+    int port;
+    
+    Socket socket = null;
+    ObjectInputStream in = null;
+    ObjectOutputStream out = null;
+    
+    boolean enabled = true;
+    boolean connected = false;
+    
+    int reconnectionTime;
+    
+    private Set<ClientListener> listeners = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    
+    public SocketClient(String host, int port, int reconnectionTime) {
+        this.host = host;
+        this.port = port;
+        this.reconnectionTime = reconnectionTime;
+    }
+    
+    public void addListener(ClientListener toAdd) {
+        this.listeners.add(toAdd);
+    }
+    
+    public boolean isConnected() {
+        return this.connected;
+    }
+    
+    public boolean isClosed() {
+        return !this.enabled;
+    }
+    
+    public int getReconnectionTime() {
+        return this.reconnectionTime;
+    }
+    
+    public Socket getSocket() {
+        return this.socket;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            this.connect();
+        } catch (IOException ex) {
+            this.reconnect();
+        }
+        while(this.enabled) {
+            try {
+                Object packet = this.in.readObject();
+                for(ClientListener listener : this.listeners)
+                    listener.onReceivePacket(packet);
+            } catch (ClassNotFoundException ex) {
+                for(ClientListener listener : this.listeners)
+                    listener.onInvalidPacket();
+            } catch (IOException ex) {
+                if (this.enabled) {
+                    for(ClientListener listener : this.listeners)
+                        listener.onDisconnection();
+                    this.reconnect();
+                }
+            }
+        }
+    }
+    public void connect() throws IOException {
+        this.socket = new Socket(this.host, this.port);
+        this.socket.setSoTimeout(0);
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+        this.connected = true;
+        for(ClientListener listener : this.listeners)
+            listener.onConnection((InetSocketAddress)this.socket.getRemoteSocketAddress());
+    }
+    
+    public void reconnect() {
+        this.connected = false;
+        try {
+            if (this.out != null) this.out.close();
+            if (this.in != null) this.in.close();
+            if (this.socket != null) this.socket.close();
+            this.out = null;
+            this.in = null;
+            this.socket = null;
+        } catch (IOException ex) {}
+        
+        while (this.enabled) {
+            for(ClientListener listener : this.listeners)
+                listener.onReconnection();
+            try {
+                Thread.sleep(this.reconnectionTime);
+            } catch (InterruptedException ex) {}
+            try {
+                this.connect();
+                return;
+            } catch (IOException ex) {}
+        }
+    }
+    
+    public boolean sendObject(Object object) {   
+        if (this.socket != null && this.out != null) {
+            try {
+                this.out.writeObject(object);
+                this.out.flush();
+                for(ClientListener listener : this.listeners)
+                    listener.onSendPacket(object, true);
+                return true;
+            } catch (IOException ex) {} 
+        }
+        for(ClientListener listener : this.listeners)
+            listener.onSendPacket(object, false);
+        return false;
+    }
+    
+    public void close() {
+        if (!this.enabled) return;
+        this.enabled = false;
+        try {
+            if (out != null) this.out.close();
+            if (in != null) this.in.close();
+            if (socket != null) socket.close();
+        } catch (IOException ex) {}
+        this.connected = false;
+        for(ClientListener listener : this.listeners)
+            listener.onClose();
+    }
+}
---
+++ src/main/java/com/lotzy/skcrew/spigot/sockets/SocketClientListener.java
@@ -0,0 +1,169 @@
+package com.lotzy.skcrew.spigot.sockets;
+
+import com.lotzy.skcrew.spigot.Skcrew;
+import com.lotzy.skcrew.spigot.sockets.SocketClient.ClientListener;
+import com.lotzy.skcrew.shared.sockets.data.BasePlayer;
+import com.lotzy.skcrew.shared.sockets.data.BaseServer;
+import com.lotzy.skcrew.shared.sockets.data.Utils;
+import com.lotzy.skcrew.shared.sockets.data.SpigotPlayer;
+import com.lotzy.skcrew.shared.sockets.data.Signal;
+import com.lotzy.skcrew.shared.sockets.data.SpigotServer;
+import com.lotzy.skcrew.spigot.sockets.events.PlayerConnectedProxyEvent;
+import com.lotzy.skcrew.spigot.sockets.events.PlayerConnectedServerEvent;
+import com.lotzy.skcrew.spigot.sockets.events.PlayerDisconnectedProxyEvent;
+import com.lotzy.skcrew.spigot.sockets.events.PlayerDisconnectedServerEvent;
+import com.lotzy.skcrew.spigot.sockets.events.ProxyConnectEvent;
+import com.lotzy.skcrew.spigot.sockets.events.ProxyDisconnectEvent;
+import com.lotzy.skcrew.spigot.sockets.events.ProxyReconnectEvent;
+import com.lotzy.skcrew.spigot.sockets.events.ProxyServerConnectEvent;
+import com.lotzy.skcrew.spigot.sockets.events.ProxyServerDisconnectEvent;
+import com.lotzy.skcrew.spigot.sockets.events.SignalEvent;
+import com.lotzy.skcrew.shared.sockets.packets.Packet;
+import com.lotzy.skcrew.shared.sockets.packets.PacketType;
+import com.lotzy.skcrew.shared.sockets.packets.PacketInitialConnection;
+import com.lotzy.skcrew.shared.sockets.packets.PacketOutcomingSignal;
+import com.lotzy.skcrew.shared.sockets.packets.PacketPlayerConnectedProxy;
+import com.lotzy.skcrew.shared.sockets.packets.PacketPlayerConnectedServer;
+import com.lotzy.skcrew.shared.sockets.packets.PacketPlayerDisconnectedProxy;
+import com.lotzy.skcrew.shared.sockets.packets.PacketPlayerDisconnectedServer;
+import com.lotzy.skcrew.shared.sockets.packets.PacketServerConnected;
+import com.lotzy.skcrew.shared.sockets.packets.PacketServerDisconnected;
+import com.lotzy.skcrew.shared.sockets.packets.PacketServersInfo;
+import java.net.InetSocketAddress;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.bukkit.Bukkit;
+
+public class SocketClientListener implements ClientListener {
+    boolean isConnected = false;
+    String name = null;
+    Set<SpigotServer> servers = new HashSet();
+    InetSocketAddress address = null;
+    
+    public SpigotServer getThisServer() {
+        return this.servers.stream().filter(server -> server.getName().equals(this.name)).findFirst().orElse(null);
+    }
+    
+    public Set<SpigotServer> getServers() {
+        return this.servers;
+    }
+    public Set<SpigotServer> getOnlineServers() {
+        return this.getServers().stream().filter(server -> server.isConnected()).collect(Collectors.toSet());
+    }
+    
+    private SpigotServer getServer(BaseServer server) {
+        return this.servers.stream().filter(spigotServer -> spigotServer.equals(server)).findFirst().orElse(null);
+    }
+    public SpigotServer getServer(String name) {
+        return this.servers.stream().filter(server -> server.getName().equals(name)).findFirst().orElse(null);
+    }
+        
+    public Set<SpigotPlayer> getPlayers() {
+        return this.getOnlineServers().stream()
+            .map(server -> server.getPlayers()).flatMap(players -> players.stream()).collect(Collectors.toSet());
+    }
+    public Set<SpigotPlayer> getPlayers(Set<BaseServer> servers) {
+         return this.getOnlineServers().stream().filter(server -> servers.contains(server))
+            .map(server -> server.getPlayers()).flatMap(players -> players.stream()).collect(Collectors.toSet());
+    }
+    public Set<SpigotPlayer> getPlayers(BaseServer server) {
+        return this.getOnlineServers().stream().filter(fserver -> fserver.equals(server))
+            .map(fserver -> fserver.getPlayers()).flatMap(players -> players.stream()).collect(Collectors.toSet());
+    }
+    public SpigotPlayer getPlayer(BasePlayer player) {
+        return this.getPlayers().stream().filter(pplayer -> player.equals(pplayer)).findFirst().orElse(null);
+    }
+    
+    
+    @Override
+    public void onReconnection() {
+        Bukkit.getScheduler().runTask(Skcrew.getInstance(),() -> Bukkit.getPluginManager().callEvent(new ProxyReconnectEvent()));
+    }
+    
+    @Override
+    public void onConnection(InetSocketAddress address) {
+        this.address = address;
+        Skcrew.getInstance().getSocketClient().sendObject(new PacketInitialConnection(Bukkit.getPort()));
+    }
+    
+    @Override
+    public void onDisconnection() {
+        if (isConnected) {
+            isConnected = false;
+            Bukkit.getScheduler().runTask(Skcrew.getInstance(),() -> Bukkit.getPluginManager().callEvent(new ProxyDisconnectEvent(this.address)));
+            this.servers.clear();
+            return;
+        }
+    }
+    
+    @Override
+    public void onReceivePacket(Object packet) {
+        Packet p = (Packet) packet;
+        if (!isConnected) {
+            if (p.getType() != PacketType.SERVERS_INFO) return;
+            PacketServersInfo infoPacket = (PacketServersInfo)p;
+            this.servers = infoPacket.getServers();
+            name = infoPacket.getName();
+            Bukkit.getScheduler().runTask(Skcrew.getInstance(),() -> Bukkit.getPluginManager().callEvent(new ProxyConnectEvent(this.address)));
+            isConnected = true;
+            return;
+        }
+        
+        switch (p.getType()) {
+            case SERVER_CONNECTED:{
+                PacketServerConnected packetServerConnected = (PacketServerConnected) packet;
+                SpigotServer server = this.getServer(packetServerConnected.getServer());
+                if (server == null) return;
+                server.setConnected(true);
+                Stream.of(packetServerConnected.getPlayers()).forEach(player -> new SpigotPlayer(player.getName(),player.getUUID(),server));
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(),() -> Bukkit.getPluginManager().callEvent(new ProxyServerConnectEvent(server)));
+                break;
+                }
+            case SERVER_DISCONNECTED:{
+                SpigotServer server = this.getServer(((PacketServerDisconnected) packet).getServer());
+                if (server == null) return;
+                server.setDisconnected();
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(),() -> Bukkit.getPluginManager().callEvent(new ProxyServerDisconnectEvent(server)));
+                break;
+                }
+           
+            case PLAYER_CONNECTED_PROXY:{
+                BasePlayer player = ((PacketPlayerConnectedProxy) packet).getPlayer();
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(), () -> Bukkit.getPluginManager().callEvent(new PlayerConnectedProxyEvent(Utils.BukkitPlayerFromBasePlayer(player))));
+                break;
+                }
+            case PLAYER_DISCONNECTED_PROXY:{
+                BasePlayer player = ((PacketPlayerDisconnectedProxy) packet).getPlayer();
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(), () -> Bukkit.getPluginManager().callEvent(new PlayerDisconnectedProxyEvent(Utils.BukkitPlayerFromBasePlayer(player))));
+                break;
+                }
+            
+            case PLAYER_CONNECTED_SERVER:{
+                BasePlayer player = ((PacketPlayerConnectedServer) packet).getPlayer();
+                SpigotServer server = this.getServer(((PacketPlayerConnectedServer) packet).getServer());
+                if (server == null) return;
+                new SpigotPlayer(player.getName(),player.getUUID(),server);
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(), () -> Bukkit.getPluginManager().callEvent(new PlayerConnectedServerEvent(Utils.BukkitPlayerFromBasePlayer(player), server)));
+                break;
+                }
+            case PLAYER_DISCONNECTED_SERVER:{
+                BasePlayer player = ((PacketPlayerDisconnectedServer) packet).getPlayer();
+                SpigotServer server = this.getServer(((PacketPlayerDisconnectedServer) packet).getServer());
+                if (server == null) return;
+                server.getPlayers().remove(player);
+                Bukkit.getScheduler().runTask(Skcrew.getInstance(), () -> Bukkit.getPluginManager().callEvent(new PlayerDisconnectedServerEvent(Utils.BukkitPlayerFromBasePlayer(player), server)));
+                break;
+            }
+            case OUTCOMING_SIGNAL:{
+                PacketOutcomingSignal spacket = (PacketOutcomingSignal) packet;
+                for (Signal signal : spacket.getSignals()) {
+                    Bukkit.getScheduler().runTask(Skcrew.getInstance(), () -> Bukkit.getPluginManager().callEvent(new SignalEvent(signal)));
+                }   
+            }
+            default:
+                break;
+        }
+    }
+}
