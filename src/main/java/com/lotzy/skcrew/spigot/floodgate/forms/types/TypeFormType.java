35246aac04cd7a38adc8fbedb7235a74fe28b170
Lotzyprod <trek.ru@yandex.ru>
2023-10-18 03:17:51 +0300

    removed parse ability due to long freezings

--- src/main/java/com/lotzy/skcrew/spigot/floodgate/forms/types/TypeFormType.java
+++ src/main/java/com/lotzy/skcrew/spigot/floodgate/forms/types/TypeFormType.java
@@ -5,34 +5,29 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import org.geysermc.cumulus.form.util.FormType;
-import ch.njol.skript.expressions.base.EventValueExpression;
-import ch.njol.skript.util.EnumUtils;
 
 public class TypeFormType {
     static {
-        final EnumUtils<FormType> formTypes = new EnumUtils<>(FormType.class, "form types");
         Classes.registerClass(new ClassInfo<>(FormType.class, "formtype")
             .user("form ?types?")
             .name("Form Type")
             .description("Form types (represents org.geysermc.cumulus.form.util.FormType class)")
-            .usage(formTypes.getAllNames())
             .since("3.0")
-            .defaultExpression(new EventValueExpression<>(FormType.class))
             .parser(new Parser<FormType>() {
-                    @Override
-                    public FormType parse(String s, ParseContext context) {
-                        return formTypes.parse(s);
-                    }
+                @Override
+                public FormType parse(String s, ParseContext context) {
+                    return null;
+                }
 
-                    @Override
-                    public String toString(FormType o, int flags) {
-                        return formTypes.toString(o, flags);
-                    }
+                @Override
+                public String toString(FormType o, int flags) {
+                    return o.toString().toLowerCase().replace('_', ' ')+" form";
+                }
 
-                    @Override
-                    public String toVariableNameString(FormType o) {
-                        return o.name();
-                    }
+                @Override
+                public String toVariableNameString(FormType o) {
+                    return o.toString().toLowerCase().replace('_', ' ')+" form";
+                }
             }));
     }
 }
