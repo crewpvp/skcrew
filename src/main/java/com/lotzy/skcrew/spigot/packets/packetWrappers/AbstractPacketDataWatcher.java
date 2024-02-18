14c78abbd65828d5db4577cb3c370a4f5a73b638
Lotzyprod <trek.ru@yandex.ru>
2024-02-18 03:21:12 +0300

    removed unused imports

--- src/main/java/com/lotzy/skcrew/spigot/packets/packetWrappers/AbstractPacketDataWatcher.java
+++ src/main/java/com/lotzy/skcrew/spigot/packets/packetWrappers/AbstractPacketDataWatcher.java
@@ -1,8 +1,6 @@
 package com.lotzy.skcrew.spigot.packets.packetWrappers;
 
-import io.netty.buffer.ByteBuf;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 public interface AbstractPacketDataWatcher {
     
