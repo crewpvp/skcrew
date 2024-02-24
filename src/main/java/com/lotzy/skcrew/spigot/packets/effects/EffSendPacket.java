cabf1f2dfd456c54acc6a403485bfea599086221
Lotzyprod <trek.ru@yandex.ru>
2024-02-24 16:09:47 +0300

    added pattern to send packets without call the event

--- src/main/java/com/lotzy/skcrew/spigot/packets/effects/EffSendPacket.java
+++ src/main/java/com/lotzy/skcrew/spigot/packets/effects/EffSendPacket.java
@@ -22,16 +22,20 @@ public class EffSendPacket extends Effect {
 
     static {
         Skript.registerEffect(EffSendPacket.class, 
-            "send packet %packets% to %players%");
+            "send packet %packets% to %players%",
+            "send packet %packets% to %players% without [(trigger|call)[ing]] [the] event",
+            "send packet %packets% without [(trigger|call)[ing]] [the] event to %players%");
     }
     
     private Expression<Object> packets;
     private Expression<Player> players;
+    private boolean withoutCalling;
     
     @Override
     public boolean init(Expression<?>[] expr, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parser) {
         packets = (Expression<Object>)expr[0];
         players = (Expression<Player>)expr[1];
+        withoutCalling = matchedPattern > 0;
         return true;
     }
     
@@ -42,6 +46,10 @@ public class EffSendPacket extends Effect {
  
     @Override
     protected void execute(Event e) {
-        PacketReflection.sendPacket(Arrays.asList(players.getArray(e)), Arrays.asList(packets.getArray(e)));
+        if (withoutCalling) {
+            PacketReflection.sendPacketWithoutEvent(Arrays.asList(players.getArray(e)), Arrays.asList(packets.getArray(e)));
+        } else {
+            PacketReflection.sendPacket(Arrays.asList(players.getArray(e)), Arrays.asList(packets.getArray(e)));
+        }
     }
 }
