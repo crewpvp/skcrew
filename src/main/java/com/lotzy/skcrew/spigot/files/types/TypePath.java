634401f39e0bc72785dba26f16c23b3acdcd186f
Lotzyprod <trek.ru@yandex.ru>
2024-02-24 16:32:31 +0300

    added serialization

--- src/main/java/com/lotzy/skcrew/spigot/files/types/TypePath.java
+++ src/main/java/com/lotzy/skcrew/spigot/files/types/TypePath.java
@@ -3,9 +3,13 @@ package com.lotzy.skcrew.spigot.files.types;
 import java.nio.file.Path;
 import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
+import ch.njol.skript.classes.Serializer;
 import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import ch.njol.yggdrasil.Fields;
+import java.io.StreamCorruptedException;
+import java.nio.file.Paths;
 
 public class TypePath {
 
@@ -38,6 +42,38 @@ public class TypePath {
                     return path.toString();
                 }
 
+            }).serializer(new Serializer<Path>() {
+                @Override
+                public Fields serialize(final Path path) {
+                    final Fields f = new Fields();
+                    f.putObject("path", path.toString());
+                    return f;
+                }
+
+                @Override
+                public void deserialize(final Path o, final Fields f) {
+                    assert false;
+                }
+
+                @Override
+                public Path deserialize(final Fields f) throws StreamCorruptedException {
+                    try {
+                        String name = f.getObject("path", String.class);
+                        return Paths.get(name);
+                    } catch (Exception ex) {
+                        return null;
+                    }
+                }
+
+                @Override
+                public boolean canBeInstantiated() {
+                    return false; // no nullary constructor - also, saving the location manually prevents errors should Location ever be changed
+                }
+
+                @Override
+                public boolean mustSyncDeserialization() {
+                    return true;
+                }
             }));
     }
 }
